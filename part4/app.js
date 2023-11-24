90a024d20cae0ce3b4ba47f326d0a9289a87bd7a
Vladana Ignjatic <vladana.ignjatic@develabs.net>
2023-11-24 12:49:11 +0100

    Add api/users (4.15-4.16)

--- part4/app.js
+++ part4/app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+require('express-async-errors')
+
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
@@ -19,8 +24,14 @@ mongoose.connect(mongoUrl)
 	})
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
---
+++ part4/tests/users_api.test.js
@@ -0,0 +1,138 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const helper = require('./helper_functions')
+const app = require('../app')
+const api = supertest(app)
+const User = require('../models/user')
+
+describe('ensure invalid users are not created', () => {
+
+	beforeEach(async () => {
+	  await User.deleteMany({})
+
+	  const passwordHash = await bcrypt.hash('secret', 10)
+	  const user = new User({ username: 'root', passwordHash })
+
+	  await user.save()
+	})
+
+	test('when creating user without username', async () => {
+
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			name: 'Matti Luukkainen',
+			password: 'salainen'
+		}
+
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+			.expect('Content-Type', /application\/json/)
+
+		expect(result.body.error).toContain('User validation failed: username: Path `username` is required.')
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+	})
+
+	test('when creating user with short username', async () => {
+
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			username: 'ma',
+			name: 'Matti Luukkainen',
+			password: 'salainen'
+		}
+
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+			.expect('Content-Type', /application\/json/)
+
+		expect(result.body.error).toContain('User validation failed: username: Path `username` (`ma`) is shorter than the minimum allowed length (3).')
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+	})
+
+	test('when creating user with username that already exists', async () => {
+
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			username: 'root',
+			name: 'Matti Luukkainen',
+			password: 'salainen'
+		}
+
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+			.expect('Content-Type', /application\/json/)
+
+		expect(result.body.error).toContain('User validation failed: username: Error, expected `username` to be unique. Value: `root`')
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+	})
+
+	test('when creating user without password', async () => {
+
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			username: 'matti',
+			name: 'Matti Luukkainen'
+		}
+
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+			.expect('Content-Type', /application\/json/)
+
+		expect(result.body.error).toContain('Password is required and it should be at least 3 characters long.')
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+	})
+
+	test('when creating user with short password', async () => {
+
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			username: 'matti',
+			name: 'Matti Luukkainen',
+			password: 'no'
+		}
+
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+			.expect('Content-Type', /application\/json/)
+
+		expect(result.body.error).toContain('Password is required and it should be at least 3 characters long.')
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+	})
+
+})
+
+afterAll(async () => {
+	await mongoose.connection.close()
+})
