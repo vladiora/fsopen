6c2dc079d0350a3ab0960001f0f6dd2f96e3c0f2
Vladana Ignjatic <vladana.ignjatic@develabs.net>
2023-12-04 23:03:16 +0100

    Implement a custom hook useCountry (7.7)

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react'
+import countryService from './services/countries'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+
+    if (name)
+      countryService.getCountry(name).then(returnedCountry => {
+        setCountry({...returnedCountry, found: true})
+      }).catch(() => {
+        setCountry({found: false})
+      })
+    else
+      setCountry(null)
+
+  }, [name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.name.common} </h3>
+      <div>capital {country.capital} </div>
+      <div>population {country.population}</div>
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
